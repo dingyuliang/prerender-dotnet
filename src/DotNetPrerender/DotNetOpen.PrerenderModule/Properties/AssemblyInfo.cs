89634f3007460caec1cf410ac3094df64717c33e
dingyuliang <dingyuliang@msn.com>
2017-02-11 09:42:26 -0800

    Added nuget-pack command. Pack DotNetOpen.PrerenderModule 1.0.1

--- src/DotNetPrerender/DotNetOpen.PrerenderModule/Properties/AssemblyInfo.cs
+++ src/DotNetPrerender/DotNetOpen.PrerenderModule/Properties/AssemblyInfo.cs
@@ -9,31 +9,9 @@ using System.Web;
 // associated with an assembly.
 [assembly: AssemblyTitle("DotNetOpen.PrerenderModule")]
 [assembly: AssemblyDescription("ASP.NET Middleware for Prerender.io")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DotNetOpen.PrerenderModule")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("d6c66972-bae0-4e00-92f3-6299472c8200")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: PreApplicationStartMethodAttribute(typeof(PrerenderPreApplicationStart), PrerenderPreApplicationStart.StartMethodName)]
---
+++ src/DotNetPrerender/DotNetOpen.PrerenderModule/nuget-pack.cmd
@@ -0,0 +1,1 @@
+nuget pack DotNetOpen.PrerenderModule.csproj
