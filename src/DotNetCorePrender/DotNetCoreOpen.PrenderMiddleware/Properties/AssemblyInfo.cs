53dc501ea4c81d9ba5a19c83f38215321aad085c
dingyuliang <dingyuliang@msn.com>
2017-02-11 23:01:33 -0800

    .NET Core - renamed project typo

--- src/DotNetCorePrender/DotNetCoreOpen.PrenderMiddleware/Properties/AssemblyInfo.cs
+++ src/DotNetCorePrender/DotNetCoreOpen.PrenderMiddleware/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DotNetCoreOpen.PrenderMiddleware")]
+[assembly: AssemblyProduct("DotNetCoreOpen.PrerenderMiddleware")]
 [assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
